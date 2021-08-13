80585d2f27ecc87ec847d659dab51064ae182877
Esteban BA <esteban_misael@hotmail.com>
2021-08-13 15:01:17 -0500

    Se agrego el ejercicio 16

---
+++ 1_EjerciciosJava/Utils.java
@@ -0,0 +1,21 @@
+
+public class Utils {
+	int resultado=0;
+	double resultado2=0;
+	public int sumar (int n1, int n2) {
+		resultado=n1+n2;
+		return resultado;
+	}
+	public int restar (int n1, int n2) {
+		resultado=n1-n2;
+		return resultado;
+	}
+	public int multiplicar (int n1, int n2) {
+		resultado=n1*n2;
+		return resultado;
+	}
+	public double division (double n1_1, double n2_1) {
+		resultado2=n1_1/n2_1;
+		return resultado;
+	}
+}
